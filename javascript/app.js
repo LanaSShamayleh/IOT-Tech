36ab9f3c2a58a2d727fa07fa91955a75fa55b955
Lana Salah Shamaileh <shamaylehlana62@gmail.com>
2021-03-22 14:07:08 +0200

    Lab6a Edit

--- javascript/app.js
+++ javascript/app.js
@@ -1,79 +1,99 @@
+
+// Welcome Message 
+
+console.log('WELCOME TO TECHNOLOGY WORLD, PLEASE ! IF YOU ARE A NEW VISITOR , DO NOT HESITATE TO REGISTER TO INFORM YOU WITH  OUR NEW PRODUCTS ');
+
+
 for (var n = 0; n < 3; n++) {
     alert("THE WEBSITE IS ONLY FOR THE OFFICER AND CLIENT AND THEY HAVE THEIR OWN PORTALS IN THE BOTTOM OF THE WEBSITE, SO PLEASE DEAR! SELECT THE TYPE OF YOUR PORTAL IN THE FOLLOWING ASKING BOX ");
 }
 
 
+// First Part - To Know the visitor type
+
+
 var VisitorType = prompt("Who are you ?");
 
 
-while (VisitorType !== "client" && VisitorType !== "officer") {
-    VisitorType = prompt(' To See this page , You must be an officer or a Client . So , You can only write client or officer ');
+var getcorrectvisitorType = function () {
+
+    while (VisitorType !== "client" && VisitorType !== "officer") {
+        VisitorType = prompt(' To See this page , You must be an officer or a Client . So , You can only write client or officer ');
+    }
+
 }
 
+getcorrectvisitorType();
 
 var NumberOfPortals = prompt('how many portals do you want?');
 
+var getNumbersofPortals = function () {
 
-if (NumberOfPortals > 1) {
+    if (NumberOfPortals > 1) {
 
-    var portals = '<button class="block">User Portal</button><br>';
-    var result = '';
-    for (var s = 0; s < NumberOfPortals; s++) {
+        var portals = '<button class="block">User Portal</button><br>';
+        var result = '';
 
-        result += portals;
+        for (var s = 0; s < NumberOfPortals; s++) {
+
+            result += portals;
+        }
+
+
+
+    } else {
+
+        var portals = '<button class="block">User Portal</button><br>';
+        var result = '';
+
+        for (var s = 0; s < NumberOfPortals; s++) {
+
+            result += portals;
+        }
+
     }
 
-    document.write(result);
-
-} else {
-
-    var portals = '<button class="block">User Portal</button><br>';
-    var result = '';
-
-    for (var s = 0; s < NumberOfPortals; s++) {
-
-        result += portals;
-    }
-
-    document.write(result);
-
-
+    return result
 
 }
 
+// Second Part - To get a Visitor the informations which may used to log into own's portal 
 
 
-if (VisitorType == 'officer') {
+var getVistorInformation = function () {
 
-    var myWindow = window.open("", "MsgWindow", "width=400,height=400");
+    if (VisitorType == 'officer') {
 
-    var Msg = 'THIS IS OFFICER WINDOW . CLICK ON YOUR PORTAL BUTTON THEN ENTER YOUR BRANCH , JOB NUMBER AND PASSWORD.  ';
-
-    myWindow.document.write(Msg.bold());
-
-
-} else {
-
-
-    if (VisitorType == 'client') {
         var myWindow = window.open("", "MsgWindow", "width=400,height=400");
-        var Msg = 'THIS IS CLIENT WINDOW . CLICK ON YOUR PORTAL BUTTON THEN ENTER YOUR USERNAME AND PASSWORD. ';
+
+        var Msg = 'THIS IS OFFICER WINDOW . CLICK ON YOUR PORTAL BUTTON THEN ENTER YOUR BRANCH , JOB NUMBER AND PASSWORD.  ';
 
         myWindow.document.write(Msg.bold());
+
 
     } else {
 
 
-        var myWindow = window.open("", "MsgWindow", "width=400,height=400");
-        var Msg = 'THIS IS A WELCOME WINDOW . IF YOU NEED THIS PAGE , PLEASE REGISTER AS A CLINENT IN THIS PAGE :"IOT-ENTELLIGENCE-REGEISTERTION-PAGE.SRC.JO/?chlang=eng" ';
+        if (VisitorType == 'client') {
+            var myWindow = window.open("", "MsgWindow", "width=400,height=400");
+            var Msg = 'THIS IS CLIENT WINDOW . CLICK ON YOUR PORTAL BUTTON THEN ENTER YOUR USERNAME AND PASSWORD. ';
 
-        myWindow.document.write(Msg.bold());
+            myWindow.document.write(Msg.bold());
 
+        } else {
+
+
+            var myWindow = window.open("", "MsgWindow", "width=400,height=400");
+            var Msg = 'THIS IS A WELCOME WINDOW . IF YOU NEED THIS PAGE , PLEASE REGISTER AS A CLINENT IN THIS PAGE :"IOT-ENTELLIGENCE-REGEISTERTION-PAGE.SRC.JO/?chlang=eng" ';
+
+            myWindow.document.write(Msg.bold());
+
+        }
     }
 }
 
+getVistorInformation();
 
-console.log('WELCOME TO TECHNOLOGY WORLD, PLEASE ! IF YOU ARE A NEW VISITOR , DO NOT HESITATE TO REGISTER TO INFORM YOU WITH  OUR NEW PRODUCTS ');
 
 
 
